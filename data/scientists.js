c6f8b101f949b336bc6f3f8db1a39f8190bce84a
Paul Brubaker <pbrubak@gmail.com>
2021-08-18 20:30:54 -0700

    add image urls

--- data/scientists.js
+++ data/scientists.js
@@ -2,32 +2,44 @@ const data = [
   {
     name: 'Ada Lovelace',
     living: false,
-    specialties_id: 1
+    specialties_id: 1,
+    img_url: './data/assets/ada_lovelace.jpg'
   },
   {
     name: 'Alan Turing',
     living: false,
-    specialties_id: 2
+    specialties_id: 2,
+    img_url: 'data/assets/220px-Alan_Turing_Aged_16.jpg'
   },
   {
     name: 'Donald Knuth',
     living: true,
-    specialties_id: 3
+    specialties_id: 3,
+    img_url: 'data/assets/192px-KnuthAtOpenContentAlliance.jpg'
   },
   {
     name: 'Yukihiro Matsumoto',
     living: true,
-    specialties_id: 3
+    specialties_id: 3,
+    img_url: 'data/assets/Yukihiro_Matsumoto.JPG'
   },
   { 
     name: 'Sophie Wilson',
     living: true,
-    specialties_id: 4
+    specialties_id: 4,
+    img_url: 'data/assets/220px-Sophie_Wilson.jpg'
   },
   {
     name: 'Haskell Curry',
     living: false,
-    specialties_id: 1
+    specialties_id: 1,
+    img_url: 'data/assets/220px-HaskellBCurry.jpg'
+  },
+  {
+    name: 'Juli-son',
+    living: true,
+    specialties_id: 2,
+    img_url: 'data/assets/juli-son-lego-astronaut.jpeg'
   }
 ];
 
